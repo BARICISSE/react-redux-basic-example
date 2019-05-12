49aa4ad4e1d522060433e0db3f2cc6077fbe1640
BARICISSE <ousmane.baricisse@mail.mcgil.ca>
2019-05-12 01:31:56 -0400

    MenuBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-import Counter from './containers/Counter/Counter';
+import Layout from './containers/Layout/Layout'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Counter />
+       <Layout />
 </div>
     );
   }
---
+++ src/components/calculatorTable/TableForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+const nums = [
+  ['C', '+/-', '%', '÷'],
+  [7, 8, 9, 'x'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+']
+]
+export class TableForm extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default TableForm
